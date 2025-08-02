2e9c9cbcd7d6d23da959b3a2214f4993cc6c9139
vikranth007 <djvikranth@gmail.com>
2025-08-02 15:19:54 +0530

    streamlit

--- streamlit_regression.py
+++ streamlit_regression.py
@@ -6,35 +6,35 @@ import pandas as pd
 import pickle
 
 # --- Load the trained model and preprocessing tools ---
+model = tf.keras.models.load_model('regression_model.h5')
 
-model = tf.keras.models.load_model('model.h5')
-
-with open('label_encoder_gender.pkl', 'rb') as file:
+with open('regression_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
-with open('one_hot_encoder_geo.pkl', 'rb') as file:
+with open('regression_ohe.pkl', 'rb') as file:
     one_hot_encoder_geo = pickle.load(file)
 
-with open('scaler.pkl', 'rb') as file:
+with open('regression_scaler.pkl', 'rb') as file:
     scaler = pickle.load(file) 
 
 # --- Streamlit UI ---
-
-st.title("Customer Churn Prediction")
+st.title("💼 Customer Churn Salary Prediction App")
+st.subheader("Enter Customer Details:")
 
 geography = st.selectbox('Geography', one_hot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
 credit_score = st.number_input('Credit Score')
-estimated_salary = st.number_input('Estimated Salary')
+exited = st.selectbox("Exited", [0, 1])
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number Of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
 
-# --- Prepare the input data ---
-
+# --- Predict Button ---
+st.button("Predict Salary")
+    # --- Prepare the input data ---
 input_data = pd.DataFrame({
     'CreditScore': [credit_score],
     'Gender': [label_encoder_gender.transform([gender])[0]],
@@ -44,7 +44,7 @@ input_data = pd.DataFrame({
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
+    'Exited': [exited]
 })
 
 # One-hot encode Geography
@@ -59,10 +59,7 @@ input_data_scaled = scaler.transform(input_data)
 
 # --- Make prediction ---
 prediction = model.predict(input_data_scaled)
-prediction_proba = prediction[0][0]
+prediction_salary = prediction[0][0]
 
 # --- Show result ---
-if prediction_proba > 0.5:
-    st.error(f'🔴 The customer is likely to churn. (Probability: {prediction_proba:.2f})')
-else:
-    st.success(f'🟢 The customer is not likely to churn. (Probability: {prediction_proba:.2f})')
+st.success(f"💰 Predicted Estimated Salary: **${prediction_salary:,.2f}**")
